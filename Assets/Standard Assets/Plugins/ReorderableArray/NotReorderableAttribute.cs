0910f1e4f3e772bc57a5d196ba42e10c7942741f
Moenenn <moenen6@gmail.com>
2021-09-07 13:03:01 +0800

    Init for v1.x.x

--- Assets/Standard Assets/Plugins/ReorderableArray/NotReorderableAttribute.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace UnityEngine {
-
-	[System.AttributeUsage(System.AttributeTargets.Field, Inherited = true, AllowMultiple = false)]
-	public class NotReorderableAttribute : PropertyAttribute {}
-
-}
-
-
--- Assets/Standard Assets/UI Gadget/Editor/Script/RaycastBlockerEditor.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace UIGadget.Editor {
-	using UnityEditor;
-	[CustomEditor(typeof(RaycastBlocker), true), CanEditMultipleObjects]
-	public class RaycastBlockerEditor : Editor {
-		public override void OnInspectorGUI () { }
-	}
-}
